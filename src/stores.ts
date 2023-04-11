45417158924f8b75044ee26887c4edb55ee97c68
effervescent000 <kislio01@gmail.com>
2023-04-10 21:38:02 -0400

    add some extra stores

--- src/stores.ts
+++ src/stores.ts
@@ -1,11 +1,37 @@
-import { get, writable } from 'svelte/store';
+import { writable } from 'svelte/store';
 
-import type { IItem } from './typing/interfaces';
+import type { IItem, IRecipe, IRequirement } from './typing/interfaces';
 
 import { createStoreFuncs } from './utils/store-utils';
 
 export const itemStore = () => {
 	const store = writable<IItem[]>([]);
+
+	const { append, remove, replace } = createStoreFuncs(store);
+
+	return {
+		subscribe: store.subscribe,
+		append,
+		remove,
+		replace
+	};
+};
+
+export const recipeStore = () => {
+	const store = writable<IRecipe[]>([]);
+
+	const { append, remove, replace } = createStoreFuncs(store);
+
+	return {
+		subscribe: store.subscribe,
+		append,
+		remove,
+		replace
+	};
+};
+
+export const requirementsStore = () => {
+	const store = writable<IRequirement[]>([]);
 
 	const { append, remove, replace } = createStoreFuncs(store);
 
