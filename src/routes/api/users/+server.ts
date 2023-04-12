dd729eb3334129a5a3a25e3c72b09578a873ee36
effervescent000 <kislio01@gmail.com>
2023-04-12 19:12:00 -0400

    set cookie in login route

--- src/routes/api/users/+server.ts
+++ src/routes/api/users/+server.ts
@@ -5,12 +5,18 @@ import type { IUser } from '../../../typing/interfaces.js';
 import { db } from '../../../db.server.js';
 import { wrapData } from '../../../utils/general-utils.js';
 
-export const POST = async ({ request }) => {
+export const POST = async ({ request, cookies }) => {
 	const data = await request.formData();
 	const username = data.get('username')?.toString();
 
 	if (username) {
-		const user = await db.user.create({ data: { name: username } });
+		let user;
+		user = await db.user.findFirst({ where: { name: username } });
+		if (!user) {
+			user = await db.user.create({ data: { name: username } });
+		}
+		cookies.set('userId', user.id.toString());
+
 		return json(wrapData<IUser>(user));
 	}
 	return json(null);
