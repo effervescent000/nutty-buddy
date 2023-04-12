6b23d059b675bddbc8e06edcc7354bbcf9b01dc8
effervescent000 <kislio01@gmail.com>
2023-04-12 19:00:04 -0400

    try with a vite variable

--- src/utils/api-utils.ts
+++ src/utils/api-utils.ts
@@ -1,17 +1,33 @@
-import { PUBLIC_ROOT_URL } from '$env/static/public';
+import { json } from '@sveltejs/kit';
 
 export const GET = async (endpoint: string) => {
-	const response = await fetch(`${PUBLIC_ROOT_URL}/api/${endpoint}`, {
-		credentials: 'include'
-	});
-	return await response.json();
+	try {
+		const response = await fetch(
+			`${import.meta.env.VITE_ROOT_URL}/api/${endpoint}`,
+			{
+				credentials: 'include'
+			}
+		);
+		return await response.json();
+	} catch (error) {
+		console.log(error);
+		return json(error);
+	}
 };
 
 export const POST = async (endpoint: string, body: FormData) => {
-	const response = await fetch(`${PUBLIC_ROOT_URL}/api/${endpoint}`, {
-		credentials: 'include',
-		method: 'post',
-		body
-	});
-	return await response.json();
+	try {
+		const response = await fetch(
+			`${import.meta.env.VITE_ROOT_URL}/api/${endpoint}`,
+			{
+				credentials: 'include',
+				method: 'post',
+				body
+			}
+		);
+		return await response.json();
+	} catch (error) {
+		console.log(error);
+		return json(error);
+	}
 };
