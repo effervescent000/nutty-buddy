0fba7e09c837b2663513058c99954a09779369e0
effervescent000 <kislio01@gmail.com>
2023-04-12 20:24:12 -0400

    try not using vite env variables again

--- src/utils/api-utils.ts
+++ src/utils/api-utils.ts
@@ -1,13 +1,11 @@
+import { PUBLIC_ROOT_URL } from '$env/static/public';
 import { json } from '@sveltejs/kit';
 
 export const GET = async (endpoint: string) => {
 	try {
-		const response = await fetch(
-			`${import.meta.env.VITE_ROOT_URL}/api/${endpoint}`,
-			{
-				credentials: 'include'
-			}
-		);
+		const response = await fetch(`${PUBLIC_ROOT_URL}/api/${endpoint}`, {
+			credentials: 'include'
+		});
 		return await response.json();
 	} catch (error) {
 		console.log(error);
@@ -17,14 +15,11 @@ export const GET = async (endpoint: string) => {
 
 export const POST = async (endpoint: string, body: FormData) => {
 	try {
-		const response = await fetch(
-			`${import.meta.env.VITE_ROOT_URL}/api/${endpoint}`,
-			{
-				credentials: 'include',
-				method: 'post',
-				body
-			}
-		);
+		const response = await fetch(`${PUBLIC_ROOT_URL}/api/${endpoint}`, {
+			credentials: 'include',
+			method: 'post',
+			body
+		});
 		return await response.json();
 	} catch (error) {
 		console.log(error);
