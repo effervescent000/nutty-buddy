45bc8887676c415e36130b705d661697a7b4edab
effervescent000 <kislio01@gmail.com>
2023-04-14 16:29:30 -0400

    add minimal requirement card

---
+++ src/components/requirements/requirement-card.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { IRequirementRead } from '../../typing/interfaces';
+	import Checkbox from '../common/checkbox.svelte';
+
+	// PROPS
+	export let requirement: IRequirementRead;
+
+	// STATE
+
+	// LOGIC
+</script>
+
+<div data-testid={`req-card-${requirement.name}`}>
+	<Checkbox name="completed" initialValue={requirement.completed} />
+	<span>{requirement.name}</span>
+</div>
--- src/routes/requirements/+page.svelte
+++ src/routes/requirements/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import Button from '../../components/common/button.svelte';
 	import UncontrolledTextInput from '../../components/common/uncontrolled-text-input.svelte';
+	import RequirementCard from '../../components/requirements/requirement-card.svelte';
 
 	// PROPS
 	export let data;
@@ -10,12 +11,12 @@
 	// LOGIC
 </script>
 
-{#each data.requirements as req}
-	<span>{req.name}</span>
-{/each}
-
 <form method="post">
 	<div><span>New requirement</span></div>
 	<UncontrolledTextInput label="Name" name="req-name" testid="req-name" />
 	<Button type="submit" testid="req-submit">Save</Button>
 </form>
+
+{#each data.requirements as req}
+	<RequirementCard requirement={req} />
+{/each}
