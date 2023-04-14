f0f295e112aec39d3fcc83e49f461acc6cbfe863
effervescent000 <kislio01@gmail.com>
2023-04-14 16:19:44 -0400

    add minimal checkbox component

---
+++ src/components/common/checkbox.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	// PROPS
+	export let initialValue = false;
+	export let name: string;
+	export let label = '';
+
+	// STATE
+	let checked = initialValue;
+
+	// LOGIC
+</script>
+
+{#if label}
+	<span>{label}</span>
+{/if}
+<input type="checkbox" {name} bind:checked />
