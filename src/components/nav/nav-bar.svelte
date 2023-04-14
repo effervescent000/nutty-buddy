b2d8a2bcbf0e867bd2e269e6af2076d9b067f8cd
effervescent000 <kislio01@gmail.com>
2023-04-14 15:45:12 -0400

    add nav bar

---
+++ src/components/nav/nav-bar.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	// PROPS
+
+	import NavLink from './nav-link.svelte';
+
+	// STATE
+
+	// LOGIC
+	const LINKS: { text: string; path: string }[] = [
+		{ text: 'Item management', path: '/items' },
+		{ text: 'Method management', path: '/methods' },
+		{ text: 'Requirement management', path: '/requirements' }
+	];
+</script>
+
+<nav class="flex gap-4 text-xl">
+	{#each LINKS as link}
+		<NavLink {...link} />
+	{/each}
+</nav>
