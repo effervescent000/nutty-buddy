183d18aa691d29a9e6a70fa79aafda55f8cd401e
effervescent000 <kislio01@gmail.com>
2023-04-15 12:33:47 -0400

    update test to work on subsequent runs

--- tests/recipes.spec.ts
+++ tests/recipes.spec.ts
@@ -8,14 +8,20 @@ test.describe('tests re: recipes', () => {
 	});
 
 	test('can persist a recipe', async ({ page }) => {
-		await page.getByTestId(`component-select-${0}`).selectOption('a test item');
-		await page.getByTestId(`component-qty-${0}`).type('1');
+		const existingRecipes = (
+			await page.getByText('Recipe for 1 a test item').all()
+		).length;
+
+		await page.getByTestId(`component-select-0`).selectOption('a test item');
+		await page.getByTestId(`component-qty-0`).type('1');
 		await page.getByTestId('method').selectOption('test method');
 		await page.getByTestId('requirement-0').selectOption('test req');
-		await page.getByTestId(`output-select-${0}`).selectOption('a test item');
-		await page.getByTestId(`output-qty-${0}`).type('1');
+		await page.getByTestId(`output-select-0`).selectOption('a test item');
+		await page.getByTestId(`output-qty-0`).type('1');
 		await page.getByTestId('submit').click();
 
-		await expect(page.getByText('Recipe for 1 a test item')).toBeVisible();
+		expect(
+			(await page.getByText('Recipe for 1 a test item').all()).length
+		).toBeGreaterThan(existingRecipes);
 	});
 });
