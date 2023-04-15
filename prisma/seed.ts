93ff0369989500aa915ea961d3151e3a7cd8e819
effervescent000 <kislio01@gmail.com>
2023-04-15 12:33:19 -0400

    update seed to include a recipe

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -28,7 +28,42 @@ const main = async () => {
 			user: { connect: { id: user.id } }
 		}
 	});
-	console.log({ user, item, method, req });
+	const recipe = await prisma.recipe.create({
+		data: {
+			user: { connect: { id: user.id } },
+			method: {
+				connect: {
+					id: method.id
+				}
+			},
+			recipeRequirements: {
+				create: [
+					{
+						requirement: { connect: { id: req.id } }
+					}
+				]
+			},
+			components: {
+				create: [
+					{
+						quantity: 1,
+						item: {
+							connect: { id: item.id }
+						}
+					}
+				]
+			},
+			output: {
+				create: [
+					{
+						chance: 1,
+						item: { connect: { id: item.id } }
+					}
+				]
+			}
+		}
+	});
+	console.log({ user, item, method, req, recipe });
 };
 main()
 	.then(async () => {
